5d5a4c132b1c806b4c6faf1e8673293efc4b8d11
James Mittler II <jamesmittlerii@my.smccd.edu>
2025-09-29 16:55:11 -0700

    Initial Commit

---
+++ homework8/homework8App.swift
@@ -0,0 +1,17 @@
+//
+//  homework8App.swift
+//  homework8
+//
+//  Created by cisstudent on 9/29/25.
+//
+
+import SwiftUI
+
+@main
+struct homework8App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
