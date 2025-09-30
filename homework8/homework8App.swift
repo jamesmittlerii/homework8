8264c8f8a533c3a7853ea9e5645868eb0234cde6
James Mittler II <jamesmittlerii@my.smccd.edu>
2025-09-30 08:40:58 -0700

    comments

--- homework8/homework8App.swift
+++ homework8/homework8App.swift
@@ -1,9 +1,20 @@
-//
-//  homework8App.swift
-//  homework8
-//
-//  Created by cisstudent on 9/29/25.
-//
+/**
+
+ * __Homework 8__
+ * Jim Mittler
+ * 23 September 2025
+
+ 
+I changed the original app to use the list and section construct for the food. After selecting a menu item, I display the full picture and description.
+
+I also changed the app to load the JSON from the asset library.
+ 
+    
+ _Italic text_
+ __Bold text__
+ ~~Strikethrough text~~
+
+ */
 
 import SwiftUI
 
