1e850a7ff3e7a3cd931406653c711771137dc612
James Mittler II <jamesmittlerii@my.smccd.edu>
2025-09-29 17:56:03 -0700

    first commit

--- homework8/ContentView.swift
+++ homework8/ContentView.swift
@@ -7,15 +7,54 @@
 
 import SwiftUI
 
+let menu = Bundle.main.decode([MenuSection].self, from: "MyData")
+
+struct FoodDetail: View {
+    var body: some View {
+        ForEach(menu) { section in
+            NavigationLink(destination: {
+                VStack(alignment: .leading) {
+                    ForEach(section.items) { item in
+                        
+                        HStack {
+                            Image(item.thumbnailImage)
+                            Text(item.name)
+                        }
+                    }
+                    
+                }})
+            {
+                Text(section.name)
+            }
+
+        }
+    }
+}
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            List {
+                HStack {
+                    Image(systemName: "car.fill")
+                    Text("Cars")
+                }
+                HStack {
+                    Image(systemName: "house.fill")
+                    Text("Home")
+                }
+                HStack {
+                    Image(systemName: "figure.run")
+                    Text("Fitness")
+                }
+                NavigationLink(destination: FoodDetail()) {
+                    HStack {
+                        Image(systemName: "fork.knife")
+                        Text("Food")
+                    }
+                }
+            }
         }
-        .padding()
+
     }
 }
 
